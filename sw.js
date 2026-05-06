a45ae692fe6d9ee836b097ff21e58115faf354cc
mitchellfloor35-sudo <mitchellfloor35@gmail.com>
2026-05-06 18:14:38 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'life-v2';
+const CACHE = 'life-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
